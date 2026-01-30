c20e6f52d84b68e8f17193f7827453a479979bdf
Vinicius Melo <marcosviniciusdefrancamelo@gmail.com>
2026-01-30 16:19:54 -0300

    Começo das structs para Lista Encadeada

--- LISTAS_INT/LISTA/LISTA.C
+++ LISTAS_INT/LISTA/LISTA.C
@@ -1,3 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+    struct lista{
+        int info;
+        struct lista* prox;
+    };
+    
--- LISTAS_INT/main.c
+++ LISTAS_INT/main.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include "LISTA/LISTA.H"
 
 int main(void){
 
-    
-
+    printf("Teste\n");
     return 0;
 }
-
-//
